48c74b197f67220e4f2a474792d08b5af9d3749a
99002688 <71593713+99002688@users.noreply.github.com>
2020-09-30 18:40:58 +0530

    Assignment 1 commits

---
+++ Assignment1/include/bitmask.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include <stdbool.h>
+
+int set1 (int bit,int pos);
+int reset(int bit);
+int flip(int bit);
+//void isKthBitSet(int n, int k)
+
+
---
+++ Assignment1/include/myutils.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int factorial(int);
+int isprime(int);
+int ispalindrome(int);
+int sum(int arg_count,...);
+
